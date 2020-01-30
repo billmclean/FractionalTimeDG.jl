8b6fd9729f67174b9016a244cb574619e587be9e
William McLean <w.mclean@unsw.edu.au>
2020-01-30 12:14:07 +1100

    refactor code for uniform case

--- examples/sec_6_1.jl
+++ examples/sec_6_1.jl
@@ -1,16 +1,17 @@
+using FractionalTimeDG
 using Printf
 
 sample = [1, 2, 10, 100, 1000]
 rmax = 6
 α = 3/4
-M = 2r
+M = 2rmax
 
-function coef_uniform(sample, r, α, M)
+function coef_uniform(sample, r, α, M, version)
     Ns = length(sample) 
     H_sample = Array{Float64}(undef, r, r, Ns, M)
     N = maximum(sample) + 1
     for m = 1:M
-        H = coef_H_uniform(N, r, α, m)
+        H = coef_H_uniform(N, r, α, m, version)
         for n = 1:Ns
             H_sample[:, :, n, m] = H[:,:,sample[n]]
         end
@@ -18,13 +19,40 @@ function coef_uniform(sample, r, α, M)
     return H_sample
 end
 
-function pts_needed(sample, rmax, α, M, atol)
+function latex_display(A::Matrix{Float64})
+    m, n = size(A)
+    maxsz = maximum(abs.(A))
+    p = min(0, ceil(Integer, log10(maxsz)))
+    @printf("10^{%0d}\n", p)
+    for i = 1:m
+        for j = 1:n-1
+            @printf("%8.5f & ", 10.0^(-p) * A[i,j])
+        end
+        if i < m
+            @printf("%8.5f\\\\\n", 10.0^(-p) * A[i,n])
+        else
+            @printf("%8.5f\n", 10.0^(-p) * A[i,n])
+        end
+    end
+end
+
+H0 = coef_H0(rmax, α)
+println("H_0:")
+latex_display(H0)
+Hs = coef_uniform(sample, rmax, α, M, version)
+for n = 1:length(sample)
+    ℓ = sample[n]
+    println("H_$ℓ:")
+    latex_display(Hs[:,:,n,M])
+end
+
+function pts_needed(sample, rmax, α, M, version, atol)
     Ns = length(sample)
     first_M = fill(-1, rmax, Ns)
     err = zeros(rmax, M)
     for n = 1:Ns
         ℓ = sample[n]
-        H = coef_uniform(sample, rmax, α, M)
+        H = coef_uniform(sample, rmax, α, M, version)
         for m = 1:M-1
             ΔH = H[:,:,n,m] - H[:,:,n,M]
             for r = 1:rmax
@@ -44,31 +72,18 @@ function pts_needed(sample, rmax, α, M, atol)
 end
 
 atol = 1e-14
-first_M = pts_needed(sample, rmax, α, M, atol)
-
-function latex_display(A::Matrix{Float64})
-    m, n = size(A)
-    maxsz = maximum(abs.(A))
-    p = min(0, ceil(Integer, log10(maxsz)))
-    @printf("10^{%0d}\n", p)
-    for i = 1:m
-        for j = 1:n-1
-            @printf("%8.5f & ", 10.0^(-p) * A[i,j])
-        end
-        if i < m
-            @printf("%8.5f\\\\\n", 10.0^(-p) * A[i,n])
-        else
-            @printf("%8.5f\n", 10.0^(-p) * A[i,n])
-        end
-    end
+version = 2
+println("\nQuadrature points needed using version = ", version)
+first_M = pts_needed(sample, rmax, α, M, version, atol)
+@printf("%4s", "r")
+for ℓ in sample
+    @printf("  ℓ=%4d", ℓ)
 end
-
-H0 = coef_H0(r, α)
-println("H_0:")
-latex_display(H0)
-Hs = coef_uniform(sample, r, α, M)
-for n = 1:length(sample)
-    ℓ = sample[n]
-    println("H_$ℓ:")
-    latex_display(Hs[:,:,n,M])
+@printf("\n\n")
+for r = 1:rmax
+    @printf("%3d&", r)
+    for n = 1:length(sample)
+        @printf("%7d&", first_M[r,n])
+    end
+    @printf("\n")
 end
