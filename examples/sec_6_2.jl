e6e7ad1118974a5ae2758f06e52dc2e0b964fd70
William McLean <w.mclean@unsw.edu.au>
2020-02-13 16:13:25 +1100

    exact solution of fractional ODE

--- examples/sec_6_2.jl
+++ examples/sec_6_2.jl
@@ -1,24 +1,27 @@
 using FractionalTimeDG
+import FractionalTimeDG
 using SpecialFunctions: erfcx
 
 T = Float64
-α = 1 / parse(T, "2")
-r = 2
-M = 2r
-store = Store(α, r, M)
+two = parse(T, "2")
+α = 1 / two
+λ = 1 / two
+tmax = two
 
 Ehalf(x) = erfcx(-x)
 
+f(t) = cos(π*t)
+u0 = one(T)
+
 function u(t::T, λ::T, u0::T, f::Function, M::Integer, 
            store::Store{T}) where T <: AbstractFloat
-    y, wy = rule(store.unitlegendre[M])
+    y, wy = GaussQuadrature.legendre(T, M)
+    y .= ( y .+ 1 ) / 2
+    wy .= wy / 2
     s = zero(T)
     for m = 1:M
         s += wy[m] * Ehalf(-λ*sqrt(t)*y[m]) * f(t*(1-y[m]^2)) * y[m]
     end
-    return u0 * Ehalf(-\lambda*sqrt(t)) + 2t * s
+    return u0 * Ehalf(-λ*sqrt(t)) + 2t * s
 end
-
-N = 3
-t, U = FODEdG!(λ, tmax, f, u0, N, r, M, store)
 
