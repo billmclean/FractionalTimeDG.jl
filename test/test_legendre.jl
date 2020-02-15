b48265a5ad5e29cb7f6a2b990e5499170e27a8cc
William McLean <w.mclean@unsw.edu.au>
2020-02-15 20:30:39 +1100

    test script for evaluating piecewise polynomials

--- test/test_legendre.jl
+++ test/test_legendre.jl
@@ -23,3 +23,18 @@ err = [ dΨ[i,j] - correct_dP(τ[j])[i] for i = 1:5, j = 1:J ]
 err = [ dΨ[i,j] - dP(i-1, τ[j]) for i = 1:5, j = 1:J ]
 @test all( abs.(err) .< 1e-10 )
 
+u(t) = exp(-t) * sin(2π*t)
+α = 0.5
+rmax = 6
+Mmax = 3rmax
+ppImax = 20
+N = 15
+store = Store(α, rmax, Mmax, ppImax)
+q = parse(T, "1.5")
+t = OffsetArray(T[ (n/N)^q for n = 0:N ], 0:N)
+U = FractionalTimeDG.Fourier_Legendre_coefs(u, rmax, t, store)
+τ = range(-1, 1, length=ppImax)
+pcwise_t, pcwise_U = FractionalTimeDG.evaluate_pcwise_poly!(U, t, τ, store)
+pcwise_u = [ u(t) for t in pcwise_t ]
+err = pcwise_U - pcwise_u
+@test all( abs.(err) .< 1e-6 )
